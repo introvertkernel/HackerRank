32248dfff2fabbb8832adaac920b6076c0d835fd
introvertkernel <gurunathab@gmail.com>
2018-11-30 17:33:18 +0530

    first commit

---
+++ DS/array-left-rotation.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+   static void leftshift(int n, int d, int[] a){
+        for(int i=0;i<d;i++){
+            int swap = a[0];
+            int j;
+            for( j=0;j<n-1;j++){
+                a[j] = a[j+1];
+            }
+            a[j] = swap;
+        }
+        for(int k = 0;k<n;k++){
+            System.out.print(a[k]+" ");
+        }
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        scanner.close();
+       /* for(int i=0;i<a.length;i++){
+                System.out.println(a[i]);
+        }*/
+       // System.out.println(n +" "+ d);
+
+       leftshift(n,d,a);
+    }
+}
+
